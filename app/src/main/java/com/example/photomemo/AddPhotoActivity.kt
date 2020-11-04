6076c608e6285c71a5863742a639e698ec1cc583
Gonshow <marufage_kun.0521@ezweb.ne.jp>
2020-11-04 17:25:27 +0900

    11_4_2

--- app/src/main/java/com/example/photomemo/AddPhotoActivity.kt
+++ app/src/main/java/com/example/photomemo/AddPhotoActivity.kt
@@ -2,10 +2,30 @@ package com.example.photomemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class AddPhotoActivity : AppCompatActivity() {
+    private val pickPhotoRequestCode = 2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_photo)
+
+        val openButton: Button = findViewById(R.id.addPhotoMemoOpenButton)
+        openButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+                addCategory(Intent.CATEGORY_OPENABLE)
+                setType("image/jpeg")
+            }
+            startActivityForResult(intent, pickPhotoRequestCode)
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == pickPhotoRequestCode && resultCode == Activity.RESULT_OK) {
+            data?.data?.let {
+                val imageView = findViewById<ImageView>(R.id.addPhotoMemoImageView)
+                imageView.setImageURI(it)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/photomemo/MainActivity.kt
+++ app/src/main/java/com/example/photomemo/MainActivity.kt
@@ -4,8 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private val addActivityRequestCode = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            val intent = Intent(this@MainActivity, AddPhotoActivity::class.java)
+            startActivityForResult(intent, addActivityRequestCode)
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
