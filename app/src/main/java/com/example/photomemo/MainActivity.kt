ef04d7e1516f982f8f2bb3ccf6be0f06ba6bd23f
Gonshow <marufage_kun.0521@ezweb.ne.jp>
2020-11-18 15:19:52 +0900

    p46まで完了

--- app/src/main/java/com/example/photomemo/MainActivity.kt
+++ app/src/main/java/com/example/photomemo/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val fab = findViewById<FloatingActionButton>(R.id.addPhotoOpenButton)
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
         fab.setOnClickListener {
             val intent = Intent(this@MainActivity, AddPhotoActivity::class.java)
             startActivityForResult(intent, addActivityRequestCode)
